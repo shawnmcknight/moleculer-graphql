b7199126b3d4468be993814c09b7cb074d9ad89b
Shawn McKnight <smcknight@storis.com>
2021-08-27 15:35:58 -0400

    Basic repo setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: "es5",
+	bracketSpacing: true,
+	arrowParens: "always",
+	parser: "typescript",
+}
