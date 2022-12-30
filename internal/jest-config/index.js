d4bb3f091318a73adb6be2d6976364a8b4fdc3f8
Shawn McKnight <smcknight@storis.com>
2022-12-30 14:32:10 -0500

    Add jest config to add source maps

--- internal/jest-config/index.js
+++ internal/jest-config/index.js
@@ -1,6 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: 'ts-jest',
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				tsconfig: '<rootDir>/tsconfig.test.json',
+			},
+		],
+	},
 	testEnvironment: 'node',
 	modulePathIgnorePatterns: ['<rootDir>/dist'],
 	testMatch: [
